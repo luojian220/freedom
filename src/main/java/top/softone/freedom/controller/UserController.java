4338e10e95b7968aa3a614909198b815f560c86f
luojian <luojian220@qq.com>
2021-03-13 21:02:52 +0800

    redisson

--- src/main/java/top/softone/freedom/controller/UserController.java
+++ src/main/java/top/softone/freedom/controller/UserController.java
@@ -1,6 +1,11 @@
 package top.softone.freedom.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,18 +16,25 @@ import org.springframework.web.bind.annotation.RestController;
 import top.softone.freedom.model.User;
 import top.softone.freedom.service.UserService;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+
 /**
  * @author luojian
  * @version 1.0
  * @date: 2021年03月05日 22:00
  * @since JDK 1.8
  */
+@Slf4j
 @RestController
 @RequestMapping("userX")
 public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private RedissonClient redissonClient;
 
     @PostMapping("addUser")
     public String addUser(@RequestBody User user) {
@@ -34,8 +46,25 @@ public class UserController {
     @GetMapping("getUser")
     public String getUser(String email) {
 
+        setLock();
         User user = userService.getUser(email);
         return JSONObject.toJSONString(user);
+    }
+
+    // 测试redisson 执行不释放锁，自动续期
+    private void setLock() {
+        RLock lock = redissonClient.getLock("lock_key_111");
+        try {
+            boolean res = lock.tryLock(10, TimeUnit.SECONDS);
+            if (res) {
+                System.out.println("获取锁成功！！！");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            //注释就会自动续期
+            lock.unlock();
+        }
     }
 
     @GetMapping("login")
